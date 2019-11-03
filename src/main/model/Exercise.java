841bc4a3e41ab9fb083fed41ac528af6e6f95782
Derek Wong <derekw1@students.cs.ubc.ca>
2019-11-03 12:52:23 -0800

    Project Deliverable 9: Design Principles. Code Coverage

--- src/main/model/Exercise.java
+++ src/main/model/Exercise.java
@@ -26,21 +26,4 @@ public abstract class Exercise {
     public String getName() {
         return name;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Exercise exercise = (Exercise) o;
-        return Objects.equals(name, exercise.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
 }
