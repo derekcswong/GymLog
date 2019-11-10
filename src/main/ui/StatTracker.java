9d06ee707106d2d3d0149b7380c9ea36456ce9bb
Derek Wong <derekw1@students.cs.ubc.ca>
2019-11-10 14:11:49 -0800

    Project Deliverable 10: Design Patterns and the Web. Added simple observable pattern that prints statement when workout is added. Used external api to display quotes

--- src/main/ui/StatTracker.java
+++ src/main/ui/StatTracker.java
@@ -10,7 +10,8 @@ import java.util.Observable;
 //should i make this abstract and extend a cardio stat tracker and a weight exercise stat tracker?
 
 public class StatTracker extends Observable {
-    private HashMap<Exercise, ArrayList<Integer>> statTracker; //use this to hold all the totalweight lifted and then graph by day?
+    private HashMap<Exercise, ArrayList<Integer>> statTracker;
+    //use this to hold all the totalweight lifted and then graph by day?
 // how should i think about this here?
 
     public StatTracker() {
